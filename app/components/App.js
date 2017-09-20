f7c302d0cbc60ddb9e844c51a81806d6b0078b47
rachd <rachel.m.dorn@gmail.com>
2017-09-20 09:19:54 -0400

    add nav links and style

--- app/components/App.js
+++ app/components/App.js
@@ -3,12 +3,14 @@ const Popular = require('./Popular');
 const ReactRouter = require('react-router-dom');
 const Router = ReactRouter.BrowserRouter;
 const Route = ReactRouter.Route;
+const Nav = require("./Nav");
 
 class App extends React.Component {
     render() {
         return(
             <Router>
                 <div className="container">
+                    <Nav/>
                     <Route path='/popular' component={Popular}/>
                 </div>
             </Router>
